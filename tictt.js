29261a4c1ad7fd62a224c0ac6f6ff0322505ae94
LaurisaNeuwirth <uberlaurisa@gmail.com>
2015-10-13 14:03:59 -0400

    Re-numbered squares to correspond with idex numbers

---
+++ tictt.js
@@ -0,0 +1,19 @@
+// (1) Game starts with Button saying "Click to Play!"
+
+
+// Once clicked, button changes to "X Move"  (----> Check for click)
+
+
+// Once X has clicked (----> Check for click, check for win) button changes to "Y Move"
+
+
+// Once Y has clicked (----> Check for click, check for win) button changes to "Y Move"
+
+
+// (2) Keep track of whose click it is, where they clicked, mark that space taken.
+
+
+// (3) When a winning condition is met, button changes to "X WINS!" or "Y WINS!"
+
+
+// (4) TIE: All 9 spaces are clicked but none of the winning scenarios are met.
