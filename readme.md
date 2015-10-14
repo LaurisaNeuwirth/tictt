7c89268394562c4f81accca6741860d16f77780b
LaurisaNeuwirth <uberlaurisa@gmail.com>
2015-10-14 17:27:54 -0400

    Add switchTurns functionality
    added this function
    debugged currentPlayer

--- readme.md
+++ readme.md
@@ -1,29 +1,38 @@
 User scenarios:
 
-(Move to Logic) Game always starts with X move first.
-
 (1) X wins and O loses, with empty spaces still on the board.
 Button changes to "X Wins!" then after 5 seconds, asks "Rematch?"
 
 (2) O wins with extra spaces on the board.
 Button changes to "X Wins!" then after 5 seconds, asks "Rematch?"
 
-(3) Neither X nor O win. (How to logic that out?)
+(3) Neither X nor O win.
 Button says "TIE!" then after 5 seconds, asks "Rematch?"
+
+(4) Winning scenarios:
+Player in in 1,2,3
+Player in in 4,5,6
+Player in in 7,8,9
+Player in in 1,5,9
+Player in in 3,5,7
+
+
+(5) User clicks on an occupied space, button says "Square taken, choose abother"
 
 
 Logic Conditions:
-(1) TIE: All 9 spaces are clicked but none of the winning scenarios are met.
 
-(2) Game starts with Button saying "Click to Play!"
+(1) Game starts with Button saying "Click to Play!"
 Once clicked, button changes to "X Move"  (----> Check for click)
 Once X has clicked (----> Check for click, check for win) button changes to "Y Move"
 Once Y has clicked (----> Check for click, check for win) button changes to "Y Move"
 
-(3) When a winning condition is met, button changes to "? WINS!"
+(2) Keep track of whose click it is, where they clicked
 
-(4) Keep track of whose click it is, where they clicked
+(3) When a winning condition is met, button changes to "X WINS!" or "Y WINS!"
 
-Problems:
+(4) TIE: All 9 spaces are clicked but none of the winning scenarios are met.
+
+
 
 
--- tictt.js
+++ tictt.js
@@ -1,4 +1,78 @@
+
+// TO DO: ADD THESE TO OBJECT LATER!
+
+  // array of squares that have been clicked on
+  var currentGameState = ['', '', '', '', '', '', '', '', ''];
+
+  //keeps track of whose turn it is
+  var currentPlayer="X";
+
+  //array of winning conditions
+  var winningConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+
+// function to switch whose turn it is in thee variable called currentPlayer
+    var switchTurns = function() {
+
+      //debugging check
+      console.log('Player turn was ' + currentPlayer + 'before switching func');
+      if (currentPlayer==="X"){
+        currentPlayer="O";
+      } else {
+        currentPlayer="X";
+      }
+
+      console.log('Player turn is ' + currentPlayer + 'after switching turns');
+
+    };
+// --- END--- TO DO
+
+//when the document has been processde by browser,
+//  the jQuery Ready method will invoke the function passed to it
+$(document).ready(function(){
+
+  //get the button telling user to start playing
+  //CSS selector # look for id
+  $('#main-button').html('Click to Play!');
+
+  $('#board').on('click', function(event){
+    //when I do a click event with element with id = board
+
+    //debug print statement
+    console.log("The target of the click event was ", event.target);
+
+    //WHen there is a click event, store as squareClickedOn
+    var squareClickedOn = event.target;
+
+    //store the object id of the square that is clicked on
+    var squareClickedId = $(squareClickedOn).data('square');
+
+    //display which square click on  DEBUG
+    console.log("id is ", squareClickedId);
+
+    //filling up the array with X & Os as moves happen
+    currentGameState[squareClickedId] = currentPlayer; // game logic
+
+    //write an X into the clicked square
+    $(squareClickedOn).html(currentPlayer); // ui
+
+    //after the currentPlayer plays, invole swithTurms()
+    switchTurns();
+
+    //use find or index_of to compare two arrays
+
+  });
+
+
+
+
+});
+
+
+
+
 // (1) Game starts with Button saying "Click to Play!"
+
+  //initialize button to read "Click to Play"
 
 
 // Once clicked, button changes to "X Move"  (----> Check for click)
@@ -14,6 +88,8 @@
 
 
 // (3) When a winning condition is met, button changes to "X WINS!" or "Y WINS!"
+
+//The delay and change the button to "Rematch?"
 
 
 // (4) TIE: All 9 spaces are clicked but none of the winning scenarios are met.
