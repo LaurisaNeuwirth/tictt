64dc1d05fe69d212977677533beaf54d3269517e
LaurisaNeuwirth <uberlaurisa@gmail.com>
2015-10-15 14:09:24 -0400

    Add isWinner and getWinner, game medcahnics finsihed & tested

--- tictt.js
+++ tictt.js
@@ -1,34 +1,85 @@
 
 // TO DO: ADD THESE TO OBJECT LATER!
 
-  // array of squares that have been clicked on
-  var currentGameState = ['', '', '', '', '', '', '', '', ''];
+// array of squares that have been clicked on
+var currentGameState = ['', '', '', '', '', '', '', '', ''];
 
-  //keeps track of whose turn it is
-  var currentPlayer="X";
+//keeps track of whose turn it is
+var currentPlayer="X";
 
-  //array of winning conditions
-  var winningConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+//array of winning conditions
+var winningConditions = [[0,1,2],
+                         [3,4,5],
+                         [6,7,8],
+                         [0,3,6],
+                         [1,4,7],
+                         [2,5,8],
+                         [0,4,8],
+                         [2,4,6]];
 
 // function to switch whose turn it is in thee variable called currentPlayer
-    var switchTurns = function() {
+  var switchTurns = function() {
 
-      //debugging check
-      console.log('Player turn was ' + currentPlayer + 'before switching func');
-      if (currentPlayer==="X"){
-        currentPlayer="O";
-      } else {
-        currentPlayer="X";
-      }
+    //debugging check
+    console.log('Player turn was ' + currentPlayer + 'before switching func');
+    if (currentPlayer==="X"){
+      currentPlayer="O";
+    } else {
+      currentPlayer="X";
+    }
 
-      console.log('Player turn is ' + currentPlayer + 'after switching turns');
+    console.log('Player turn is ' + currentPlayer + 'after switching turns');
 
-    };
+  };
+
+// return true or false based on whether or not a given player is the winner
+var isWinner = function(player){
+  //see if winning conditions are present in the currentGameState array
+    // With only one win condition, [0,1,2]
+    // We would compare the element at each index to the given player.
+  return winningConditions.some(function(condition){ //condition is the inside array - each specific win condition
+    return currentGameState[condition[0]] === player &&
+           currentGameState[condition[1]] === player &&
+           currentGameState[condition[2]] === player;
+  });
+};
+//write checkForWinner function
+var getWinner = function () {
+
+  //holds winner of the game
+  var winner = "";
+
+  if (isWinner("X")) {      //if x won
+    winner="X";
+  }
+  else if (isWinner("O")){  //if o won
+    winner="O";
+  }
+  else if (false) {   //if tie
+    winner="TIE";
+  } else {     //is game still in play
+    winner=null;
+  }
+  return winner;
+};//end of getWinner function
+
+
 // --- END--- TO DO
+
+// BEGIN AJAX
+///// get the whole `tttapi` object
+///// you'll need to customize
+/////  1. configuration settings (e.g. URL, method)
+/////  2. callbacks from requests≥
+
+// END AJAX
 
 //when the document has been processde by browser,
 //  the jQuery Ready method will invoke the function passed to it
 $(document).ready(function(){
+
+  debugger;
+
 
   //get the button telling user to start playing
   //CSS selector # look for id
@@ -60,29 +111,19 @@ $(document).ready(function(){
     currentGameState[squareClickedId]=currentPlayer;
 
     //debug to see if the array updates
-
+    //create a function to put index number and value of current game state array
+    //  into a console.lg for debugging
     var callbackConsoleLog=function callbackConsoleLog(currCell, index){
       console.log(index, currCell);
+    };
 
-    }
+    //call the function to display current game state array into
+    //  a console log
     currentGameState.forEach(callbackConsoleLog)
 
-
-
-    //write checkForWinner function
-    //if x won
-    //if o won
-    //if tie
-    //is game still in play
-
-
-    //after the currentPlayer plays, invole swithTurms()
+    //after the currentPlayer plays, designate other player to take a turn
     switchTurns();
-
-    //use find or index_of to compare two arrays
-
   });
-
 
 
 
